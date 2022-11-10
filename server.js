ea98306945c9c57fd2c3f11b1b5fa6df3f9900d8
walidbousnina@gmail.com <abnejib23@gmail.com>
2022-11-10 15:55:22 +0100

    upgrade com back normal dist file

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(express.static('./dist/leave-management-angular'));
 
 app.get('/*', (req, res) =>
-  res.sendFile('index.html', {root: 'dist/angular-heroku/'}),
+  res.sendFile('index.html', {root: 'dist/leave-management-angular/'}),
 );
 
 app.listen(process.env.PORT || 8070);
